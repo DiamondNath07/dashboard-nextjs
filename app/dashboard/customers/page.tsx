e269cf8f9d69cd7cb9a9acc23b54c88aeda799c0
Diamond Nathaniel <diamondnathaniel2@gmail.com>
2024-02-13 01:09:16 +0100

    dashboard app with nextjs

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+function Customers (){
+     return <h1>customers page</h1>
+}
+
+export default Customers;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>Dashbooard component</h1>;
+}
+
+export default Dashboard;
